17b6d376af2910c9affa7ca51cd40aa21431f02d
Ignacio <55958716+kiltro-dev@users.noreply.github.com>
2023-05-27 22:41:10 -0400

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Propmpts',
+};
+
+const RootLayout = ({children}) => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
